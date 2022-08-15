6a87f90fda1450013689ccc982e08804d09d900a
Himanshu Suthar <h9714348433@gmail.com>
2022-08-15 23:32:53 +0530

    Add database and creates user authentication

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
 def create_app():
     # Create an Flask app
     app = Flask(__name__)
@@ -10,11 +14,39 @@ def create_app():
     # To hash or encrypt session
     app.config["SECRET_KEY"] = "hellothisismysecret"
 
+    # Config and init database
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
+
     # Register blueprints
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
-    
+
+    # models
+    from .models import User
+
+    # Creating database
+    create_database(app)
+
+    # Allows log in and out the users
+    login_manager = LoginManager()
+    # If someone is not logged in then redirect them to auth.login
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
+def create_database(app):
+    # If database doesn't exists create it.
+    # This is not useful in production. But you can use it locally.
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database!")
--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 # Creating blueprint and linked to flask app
 views = Blueprint("views", __name__)
 
+
 @views.route("/")
 @views.route("/home")
 def home():
-    return render_template("home.html", name="Ram")
+    return render_template("home.html", name=current_user.username)
